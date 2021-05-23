ff3f3d84c83197d33851a18893a50392eb1281ee
Kruti Patel <kruti.patel0709@gmail.com>
2021-05-23 16:09:58 -0400

    add color-coded time blocks to indicate past,present,future tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,35 @@
-$("#currentDay").text(moment().format("dddd, MMM Do YYYY"));
+$(document).ready(function () {// essentially load 1)html & 2)css first.
+    //display current day & time.
+    $("#currentDay").text(moment().format("dddd, MMM Do YYYY"));
+
+    function hourTracker() {
+        //get current number of hours.
+        var currentHour = moment().hour();
+
+        // loop over time blocks
+        $(".scheduleTimeBlock").each(function () {
+            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+            console.log( blockHour, currentHour)
+
+            //check if we've moved past this time
+            if (blockHour < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("present");
+                $(this).removeClass("future");
+            }
+            else if (blockHour === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            else {
+                $(this).removeClass("past");
+                $(this).removeClass("present");
+                $(this).addClass("future");
+            }
+        })
+    }
+    hourTracker();
+})
+
+
